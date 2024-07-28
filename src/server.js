70151ba03c08328982db17af2d6f7a35189f4eb8
Tetyana Gogulya <cheburashka077@ukr.net>
2024-07-28 13:43:41 +0300

    done

--- src/server.js
+++ src/server.js
@@ -37,10 +37,8 @@ import { Contact } from './models/contact.js';
 
 
       app.get('/contacts', async (req, res) => {
-          console.log('Received request for /contacts'); //додаткове
         try {
             const contacts = await Contact.find();
-             console.log('Fetched contacts:', contacts); // Додайте цей рядок для логування даних
             res.send({ status: 200, message: 'Successfully found contacts!', data: contacts });
         } catch (error) {
             console.error(error);
@@ -49,6 +47,24 @@ import { Contact } from './models/contact.js';
         }
     }
       );
+
+      app.get('/contacts/:contactId', async (req, res) => {
+          const { contactId } = req.params;
+          try {
+              const contact = await Contact.findById(contactId);
+
+              if (contact === null) {
+                  return res
+                      .status(404)
+                      .send({ status: 404, message: 'Contact not found' });
+              }
+              res.send({ status: 200, message: `Successfully found contact with id ${contactId}`, data: contact });
+
+          } catch (error) {
+              console.error(error);
+              res.status(500).send({ message: 'Internal server error' });
+          };
+      });
 
       app.use((req, res, next) => {
           res.status(404).send({
