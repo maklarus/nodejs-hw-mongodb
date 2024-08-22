96ed5ca8e7adda6ae5faa92b195a82a1aae70464
Mykhailo <mykhailo.khariuk@gmail.com>
2024-08-22 16:48:00 +0200

    init

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,5 +1,3 @@
-
-
 export function notFoundHandler(req, res, next) {
-    res.status(404).send({ status: 404, message: 'Rout not found' });
+  res.status(404).send({ status: 404, message: 'Rout not found' });
 }
