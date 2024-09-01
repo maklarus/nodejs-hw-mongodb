72aa7f75d7e6ec65be02fd336c0b1d78aeb621b1
Tetyana Gogulya <cheburashka077@ukr.net>
2024-09-01 03:34:10 +0300

    contacts_get

--- src/server.js
+++ src/server.js
@@ -12,30 +12,25 @@ import { authenticate } from './middlewares/auth.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
+import { swaggerDocs } from "./middlewares/swaggerDocs.js";
 
+export const setupServer = () => {
 
-
-
-  export const setupServer = () => {
       const app = express();
+
+      app.use('/api-docs', swaggerDocs());
 
       app.use("/avatars", express.static(path.resolve("src", "public/avatars")));
 
       app.use(cookieParser());
 
-    app.use(
-        cors({
-            origin: 'http://localhost:3000',
-            optionsSuccessStatus: 200,
-        }),
-    );
+      app.use(cors());
 
-
-    const pino = pinoHTTP({
+      const pino = pinoHTTP({
         transport: {
             target: 'pino-pretty',
         },
-    });
+     });
 
       app.use(pino);
       app.use(express.json());
@@ -49,8 +44,8 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
       const PORT = process.env.PORT || 3000;
 
-    app.listen(PORT, () => {
+     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
-    });
+     });
 };
 
